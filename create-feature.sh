04b82dcc074c02ee1c842238384693e17076a9ff
Brendan Nolan <brendan.nolan@gmail.com>
2017-09-26 22:05:04 +0100

    Add tracking-ci resource

--- create-feature.sh
+++ create-feature.sh
@@ -2,6 +2,9 @@
 
 set -euxo
 
+git config --global user.email "brendan.nolan-ci@gmail.com"
+git config --global user.name "Brendan Nolan (CI)"
+
 pushd kubo-release
 
   version=$(git describe --tags --abbrev=0)
@@ -10,10 +13,13 @@ popd
 
 git clone kubo-release kubo-release-feature
 
+echo "feature/$version" > kubo-feature-branch
+
 pushd kubo-release-feature
 
-  git checkout -b feature/"${version}"
-  git commit -m "Create ${version} feature branch"
+  echo $(date) > test.txt
+  git add .
+  git commit -m "Test commit"
 
 popd
 
